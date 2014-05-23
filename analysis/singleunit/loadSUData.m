4fec00e58c2ff518061b509e309369e41f926a81
jasonstrawbridge <jason_strawbridge@hotmail.com>
2014-05-23 15:47:56 -0700

    trialContactBrowser : Bugfix crop window falling outside of video frame

--- analysis/singleunit/loadSUData.m
+++ analysis/singleunit/loadSUData.m
@@ -1,17 +1,9 @@
-function [CA, T, DA, contacts, params] = loadSUData(cellNum, SU);
-% 
-% SU_ConDir = ('Z:\Users\Andrew\Whisker Project\SingleUnit\ConTA\');
-% SU_TDir =   ('Z:\Users\Andrew\Whisker Project\SingleUnit\TrialArrays\');
-% SU_CADir = ('Z:\Users\Andrew\Whisker Project\SingleUnit\CellAnalysisArrays\');
-% SU_DADir = ('Z:\Users\Andrew\Whisker Project\SingleUnit\DiscrimAnalysisArrays\');
-CA = [];
+function [T, contacts, params] = loadSUData(cellNum, SU);
 
-DA = [];
- SU_ConDir = ('C:\Users\shires\Dropbox\NoiseProject\S1_singleunit\ConTA\');
- SU_TDir =   ('C:\Users\shires\Dropbox\NoiseProject\S1_singleunit\TrialArrays\');
+ SU_ConDir = ('Z:\Data\NoiseProject\S1_singleunit\ConTA\');
+ SU_TDir =   ('Z:\Data\NoiseProject\S1_singleunit\TrialArrays\');
 display(['Loading '  SU.trialArrayName{cellNum}])
-% load([SU_CADir SU.trialArrayName{cellNum}(13:end)]);
-% load([SU_DADir 'DA_' num2str(cellNum) '_' SU.trialArrayName{cellNum}(13:end)]);
+
 load([SU_TDir SU.trialArrayName{cellNum}]);
 load([SU_ConDir SU.contactsArrayName{cellNum}]);
 
